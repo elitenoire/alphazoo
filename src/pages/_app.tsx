dfd5d6fa5dfec4c829a492e45bf12e54ae8b8431
Eva Raymond <evathecoderr@outlook.com>
2022-09-27 20:00:18 +0100

    custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,13 +8,7 @@ import theme from '~/src/theme'
 
 // Custom fonts
 import '@fontsource/mitr/latin-700.css'
-import '@fontsource/wendy-one/latin.css'
-// Use local github copy because dlig and ss01
-// not working with google version
-// import '~src/assets/fonts/grandstander/index.css'
-import '@fontsource/grandstander/latin-800.css'
-
-import '~src/assets/fonts/owen-pro/index.css'
+import '~src/styles/fonts.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
