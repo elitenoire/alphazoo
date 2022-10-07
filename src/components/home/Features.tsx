7de886fc927ef05c0cee8a2ad78011f3f6c36e76
Eva Raymond <evathecoderr@outlook.com>
2022-10-07 13:08:38 +0100

    add more features

--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -1,8 +1,19 @@
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Heading, Box, List, ListItem, Text, Button, chakra } from '@chakra-ui/react'
+import { MotionPop, MagneticBox } from '~components/motion'
+
+import { ReactComponent as DinoSvg } from '~public/img/dino.svg'
+import { ReactComponent as AbcSvg } from '~public/img/abc.svg'
+import { ReactComponent as XyloSvg } from '~public/img/xylo.svg'
+import { ReactComponent as RobotSvg } from '~public/img/robot.svg'
+
+const ChakraAbc = chakra(AbcSvg)
+const ChakraXylo = chakra(XyloSvg)
+const ChakraDino = chakra(DinoSvg)
+const ChakraRobot = chakra(RobotSvg)
 
 export default function Features() {
   return (
-    <Box mt={-300}>
+    <section>
       <Heading color="brand.500" fontSize="f5xl">
         Made for Fun
       </Heading>
@@ -10,6 +21,41 @@ export default function Features() {
         We offer fun activities & games for kids to grow their knowledge and develop their cognitive
         skills.
       </Text>
-    </Box>
+      <Box pt={16} textAlign="center">
+        <List flexWrap="wrap" rowGap={12} display="flex">
+          <ListItem flex="1 1 50%">
+            <MotionPop>
+              <ChakraAbc maxW="15em" mx="auto" />
+              <Text fontSize="fxl">English Letters</Text>
+            </MotionPop>
+          </ListItem>
+          <ListItem flex="1 1 50%">
+            <MotionPop delay={0.1}>
+              <ChakraXylo maxW="15em" mx="auto" />
+              <Text fontSize="fxl">Phonics</Text>
+            </MotionPop>
+          </ListItem>
+          <ListItem flex="1 1 50%">
+            <MotionPop delay={0.15}>
+              <ChakraDino maxW="15em" mx="auto" />
+              <Text fontSize="fxl">Animal Wiki</Text>
+            </MotionPop>
+          </ListItem>
+          <ListItem flex="1 1 50%">
+            <MotionPop delay={0.2}>
+              <ChakraRobot maxW="15em" mx="auto" />
+              <Text fontSize="fxl">Puzzle</Text>
+            </MotionPop>
+          </ListItem>
+        </List>
+        <MotionPop display="inline-block" mt={12}>
+          <MagneticBox p={4}>
+            <Button borderRadius="full" size="lg">
+              Explore the App
+            </Button>
+          </MagneticBox>
+        </MotionPop>
+      </Box>
+    </section>
   )
 }
