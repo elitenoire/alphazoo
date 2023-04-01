36266d020dba218568598fc593cb26e739f383cf
Eva Raymond <evathecoderr@outlook.com>
2023-04-01 23:53:31 +0100

    💄 IMPROVE: sticky cards

--- src/components/layout/AlphabetLayout.tsx
+++ src/components/layout/AlphabetLayout.tsx
@@ -1,5 +1,6 @@
+import NextImage from 'next/image'
 import type { PropsWithChildren, ReactElement } from 'react'
-import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading, AspectRatio } from '@chakra-ui/react'
 import { MotionPop } from '~components/motion'
 
 import { getLearnLayout } from '~/src/components/layout/DefaultLayout'
@@ -8,16 +9,48 @@ export default function AlphabetLayout({ children }: PropsWithChildren) {
   return (
     <>
       {children}
-      <Box py={40}>
-        <Flex justify="center" wrap="wrap-reverse" gap={8}>
-          <Heading fontSize="f4xl" lineHeight="none" textAlign="right">
+      <Box px={4} py={40}>
+        <Flex pos="sticky" top={20} justify="center" wrap="wrap-reverse" gap={8}>
+          <Heading flex={1} fontSize="f4xl" lineHeight="none" textAlign="right">
             Play A <br /> Game
           </Heading>
-          <MotionPop pt={12}>
-            <Box w={32} h={32} bg="gray.200" rounded="circle" />
-          </MotionPop>
+          <Box flex={1} pt={12}>
+            <MotionPop>
+              <AspectRatio w="40%" minW={44} bg="brand.100" ratio={1} rounded="circle">
+                <NextImage src={`/img/cutemk.svg`} alt={`cute monkey`} fill unoptimized />
+              </AspectRatio>
+            </MotionPop>
+          </Box>
+        </Flex>
+        <Flex wrap="wrap" rowGap={16} columnGap={6} my={40} px={[null, '5%', 0]}>
+          <Box
+            pos="sticky"
+            top={40}
+            flex={['1 1 100%', null, 1]}
+            h="xs"
+            bg="gray.200"
+            rounded={['10vw', null, '8vw']}
+          />
+          <Box
+            pos="sticky"
+            top={40}
+            flex={['1 1 100%', null, 1]}
+            h="xs"
+            mt={[null, null, 16]}
+            bg="gray.200"
+            rounded={['10vw', null, '8vw']}
+          />
+          <Box
+            pos="sticky"
+            top={40}
+            flex={['1 1 100%', null, 1]}
+            h="xs"
+            bg="gray.200"
+            rounded={['10vw', null, '8vw']}
+          />
         </Flex>
       </Box>
+      <Box h={40} bg="brand.400" />
     </>
   )
 }
